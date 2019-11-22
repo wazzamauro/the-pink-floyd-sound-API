5e019080d3f7882e0fb429f25431e894a588a2aa
Mauro Nicolì <mauro.nicoli@finconsgroup.local>
2019-11-22 10:25:48 +0100

    first commit

---
+++ src/main/java/com/springframework/thepinkfloydsound/ThePinkFloydSoundApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.thepinkfloydsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePinkFloydSoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePinkFloydSoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/thepinkfloydsound/ThePinkFloydSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.thepinkfloydsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePinkFloydSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
